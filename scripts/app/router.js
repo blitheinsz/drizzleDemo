8bfb586310ee5373ae25800cafc7c931551f1b7d
keyun <huangkeyun@zhixueyun.com>
2018-04-25 09:57:49 +0800

    first commit files

---
+++ scripts/app/router.js
@@ -0,0 +1,25 @@
+module.exports = {
+    routes: {
+        todos: 'showTodos', //showTodos对应下面定义的showTodos函数
+        listbox: 'showListBox',
+        dynamic: 'showDynamic',
+        request: 'showRequest'
+    },
+
+    showTodos: function() {
+        //在名为content的Region中展示todos模块
+        return this.app.show('content', 'todos', { forceRender: false });
+    },
+    showListBox: function() {
+         //在名为content的Region中展示listbox模块
+        return this.app.show('content', 'listbox', { forceRender: true });
+    },
+    showDynamic: function() {
+    	//在名为content的Region中展示dynamic模块
+        return this.app.show('content', 'dynamic', { forceRender: true });
+    },
+    showRequest: function() {
+    	//在名为content的Region中展示request模块
+        return this.app.show('content', 'request', { forceRender: true });
+    }
+};
